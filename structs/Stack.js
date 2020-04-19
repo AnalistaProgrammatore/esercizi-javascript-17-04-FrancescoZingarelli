9c2303f123976bd8fd7e94342d0dfe07ed28ab71
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-04-19 19:35:40 +0200

    Update Stack.js

--- structs/Stack.js
+++ structs/Stack.js
@@ -27,7 +27,7 @@ class Stack {
         console.log("Stack Underflow!") 
         return
      }
-     this.container.pop()
+     return this.container.pop()                         // L'HO CORRETTO AGGIUNGENDO return !!!
   }
   peek() {
      if (isEmpty()) {
